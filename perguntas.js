1b1862e51b158b3d15c529c1c5a3fe8646d5fd35
mateux1407 <00001117485043SP@al.educacao.sp.gov.br>
2024-11-26 07:22:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'futebol',
+    'maior time do Brasil?',
+    'Palmeiras'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'futebol',
+    'melhor jogador da história?',
+    'Lionel Messi'
+)
